a64f9cb7ce1162633c174238e97679603485340a
Wesley <wesley@channelengine.com>
2021-02-11 15:20:19 +0100

    Added themeswitcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
+import ThemeSwitcher from "./modules/themeswitcher/ThemeSwitcher";
 
 function App() {
   return (
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="/">
             Navbar
           </a>
           <button
@@ -24,19 +25,19 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a className="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <a className="nav-link" href="/">
                   Link
                 </a>
               </li>
               <li className="nav-item">
                 <a
                   className="nav-link disabled"
-                  href="#"
+                  href="/"
                   tabIndex={-1}
                   aria-disabled="true"
                 >
@@ -46,7 +47,7 @@ function App() {
               <li className="nav-item dropdown">
                 <a
                   className="nav-link dropdown-toggle"
-                  href="#"
+                  href="/"
                   id="dropdown01"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
@@ -55,22 +56,23 @@ function App() {
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="dropdown01">
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="/">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="/">
                       Another action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" href="/">
                       Something else here
                     </a>
                   </li>
                 </ul>
               </li>
+              <ThemeSwitcher />
             </ul>
             <form className="d-flex">
               <input
@@ -89,7 +91,7 @@ function App() {
 
       <main className="container">
         <div className="starter-template text-center py-5 px-3">
-          <h1>Bootstrap starter template</h1>
+          <h1>Bootstrap starter template with React</h1>
           <p className="lead">
             Use this document as a way to quickly start any new project.
             <br /> All you get is this text and a mostly barebones HTML
