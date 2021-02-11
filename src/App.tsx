50014c205b00d71603bab463022da2ee628e946d
Wesley <wesley@channelengine.com>
2021-02-11 16:15:50 +0100

    Use button instead of a

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div>
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="./">
             Navbar
           </a>
           <button
@@ -25,19 +25,19 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul className="navbar-nav me-auto mb-2 mb-md-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className="nav-link active" aria-current="page" href="./">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <a className="nav-link" href="./">
                   Link
                 </a>
               </li>
               <li className="nav-item">
                 <a
                   className="nav-link disabled"
-                  href="/"
+                  href="./"
                   tabIndex={-1}
                   aria-disabled="true"
                 >
@@ -45,28 +45,27 @@ function App() {
                 </a>
               </li>
               <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
+                <button
+                  className="nav-link dropdown-toggle btn"
                   id="dropdown01"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
                   Dropdown
-                </a>
+                </button>
                 <ul className="dropdown-menu" aria-labelledby="dropdown01">
                   <li>
-                    <a className="dropdown-item" href="/">
+                    <a className="dropdown-item" href="./">
                       Action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="/">
+                    <a className="dropdown-item" href="./">
                       Another action
                     </a>
                   </li>
                   <li>
-                    <a className="dropdown-item" href="/">
+                    <a className="dropdown-item" href="./">
                       Something else here
                     </a>
                   </li>
